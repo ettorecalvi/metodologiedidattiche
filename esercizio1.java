03dd0807da6e1f302cc4f17e614267dae49a3e62
ettorecalvi <ettore.calvi@edu.unito.it>
2024-01-29 18:16:58 +0100

    Caricamento esercizi studenti

---
+++ esercizio1.java
@@ -0,0 +1,12 @@
+class esercizio1 {
+    public static void main(String[] args) {
+        int conteggio, numero;
+        conteggio=1;
+        numero=2;
+        
+        while (conteggio <= numero) {
+            System.out.println(conteggio + ", ");
+            conteggio++;
+        }
+    }
+}
---
+++ esercizio2.java
@@ -0,0 +1,9 @@
+public class esercizio2 {
+    public static void main(String[] args) {
+        int numero = 2;
+        numero += 2;
+        System.out.println(numero);
+        while (numero <= 20) {
+        }
+    }
+}
